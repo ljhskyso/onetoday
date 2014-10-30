4fc35e0df76df86c8af3c5b433b656f81217534c
ljhskyso <jl3474@cornell.edu>
2014-10-30 18:47:56 -0400

    Hack2 - Version1

---
+++ post.py
@@ -0,0 +1,21 @@
+import tweepy, time, sys
+ 
+def post_tweet(tweet_content):
+    # argfile = str(sys.argv[1])
+ 
+    #enter the corresponding information from your Twitter application:
+    CONSUMER_KEY = 'BGpaGCjm9GDTIh6faQVj24TlE'#keep the quotes, replace this with your consumer key
+    CONSUMER_SECRET = 'PV18Pv7Ewp23IUsPIS3uYikq2BbPs9Hubl9sK4zm7kCgfvAEIn'#keep the quotes, replace this with your consumer secret key
+    ACCESS_KEY = '2800992971-Cq7Yz1LE1hNvExrXl3ghe2BJY781hhzM6fZqb2G'#keep the quotes, replace this with your access token
+    ACCESS_SECRET = 'o37J0HKqEKPpfeCu9CNnXR7okW92ULCBKMHxSvxKbqzzZ'#keep the quotes, replace this with your access token secret
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+ 
+    # filename=open(argfile,'r')
+    # f=filename.readlines()
+    # filename.close()
+ 
+    # for line in f:
+    api.update_status(tweet_content)
+        # time.sleep(60)#Tweet every 60 seconds
---
+++ projects_search.py
@@ -0,0 +1,47 @@
+import json
+from apiclient.discovery import build
+
+def project_search( keyword ):
+    
+  print('call onetoday for: ' + keyword)
+    
+  service = build('onetoday', 'v1',
+            developerKey='AIzaSyChpIe6eRbzRDlgMecR3IsMBZmWb1WG2Cw')
+
+  # keyword = 'android for autism'
+
+  s_resp =  service.projects().search(
+      query=keyword
+    ).execute()
+
+  # print(s_resp)
+
+  if 'projects' in s_resp:
+      project = s_resp["projects"][0]
+
+      url = project["projectUrl"]
+      prj_name = project["tagLine"]
+      
+      # print('url:               ' + project["projectUrl"])
+      # print('tagLine:           ' + project["tagLine"])
+      # print('Description:       ' + project["unitDescriptionMultiple"])
+      # print('projectFact:       ' + project["projectFact"])
+      # print('message:           ' + project["message"])
+      # print('shortDescription:  ' + project["unitDescription"])
+  else:
+      url = 'no project available'
+      prj_name = 'null'
+      
+  return [ url, prj_name ]
+
+  
+def main():
+  keyword = '  water'
+  [s1, s2] = project_search( keyword )
+  print(s1)
+  print(s2)
+
+
+if __name__ == '__main__':
+  main()
+  
